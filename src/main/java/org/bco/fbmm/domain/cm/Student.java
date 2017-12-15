efa9df6e51fb5f975ada1a6cf4d1435d72afe444
André H. Juffer <andre.juffer@oulu.fi>
2017-12-15 16:33:28 +0200

    Initial commit.

---
+++ src/main/java/org/bco/fbmm/domain/cm/Student.java
@@ -0,0 +1,53 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2017 ajuffer.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package org.bco.fbmm.domain.cm;
+
+/**
+ * Course participant.
+ * @author ajuffer
+ */
+public class Student {
+    
+    private final StudentId studentId_;
+    
+    private Student(StudentId studentId)
+    {
+        studentId_ = studentId;
+    }
+    
+    /**
+     * Returns new student instance.
+     * @param studentId Student identifier. Must not be null.
+     * @return Student.
+     */
+    public static Student create(StudentId studentId)
+    {
+        if ( studentId == null ) {
+            throw new NullPointerException("Student: StudentId must be provided.");
+        }
+        return new Student(studentId);
+    }
+    
+}
